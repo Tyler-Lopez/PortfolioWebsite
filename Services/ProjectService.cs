de8aa9670fae5d25226cecba152145d98fd79beb
Tyler-Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2021-09-29 16:06:36 -0400

    Project update

--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -25,6 +25,17 @@ namespace Portfolio.Services
                 },
                 new Project
                 {
+                    Name = "Speed Card Game",
+                    Description = "This repository is my primary focus at present and an engaging exercise in mastering the Canvas composable. The long-term goal is to create a local multi-player game where two users may play the card game of speed against one another. The featured picture is an early-development preview towards how suit shapes are being drawn; each as a solution to the tangram puzzle.",
+                    GithubUrl = "https://github.com/Tyler-Lopez/SpeedCardGame",
+                    PictureUrl = "https://user-images.githubusercontent.com/77797048/135339050-9264c445-cbc8-4494-978c-ebf7ac25a4ee.png",
+                    Tags = new List<string[]>
+                    {
+                       new string[]{ "https://kotlinlang.org/docs/home.html", "Kotlin"}, new string[]{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"}, new string[]{ "https://medium.com/google-developer-experts/exploring-jetpack-compose-canvas-the-power-of-drawing-8cc60815babe", "Canvas"}, new string[]{ "https://dagger.dev/hilt/", "Dagger Hilt"}, new string[]{ "https://www.geeksforgeeks.org/mvvm-model-view-viewmodel-architecture-pattern-in-android/", "Model View ViewModel (MVVM)"},
+                    }
+                },
+                new Project
+                {
                     Name = "NoNumerosity!",
                     Description = "Game-based assessments used as screening questions for job applicants are not very enjoyable or effective. This Android app will automatically solve Numerosity arithmetic, used by HireVue, for you.",
                     GithubUrl = "https://github.com/Tyler-Lopez/NumerositySolver",
@@ -32,17 +43,6 @@ namespace Portfolio.Services
                     Tags = new List<string[]>
                     {
                         new string[]{ "https://kotlinlang.org/docs/home.html", "Kotlin"}, new string[]{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"}
-                    }
-                },
-                new Project
-                {
-                    Name = "Speed Card Game",
-                    Description = "This repository is my primary focus at present and an engaging exercise in mastering the Canvas composable. The long-term goal is to create a local multi-player game where two users may play the card game of speed against one another. The featured picture is an early-development preview towards how suit shapes are being drawn; each as a solution to the tangram puzzle.",
-                    GithubUrl = "https://github.com/Tyler-Lopez/SpeedCardGame",
-                    PictureUrl = "https://user-images.githubusercontent.com/77797048/135025444-b15a5620-3468-49e3-b12f-ada1b2216e99.png",
-                    Tags = new List<string[]>
-                    {
-                       new string[]{ "https://kotlinlang.org/docs/home.html", "Kotlin"}, new string[]{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"}, new string[]{ "https://medium.com/google-developer-experts/exploring-jetpack-compose-canvas-the-power-of-drawing-8cc60815babe", "Canvas"}, new string[]{ "https://dagger.dev/hilt/", "Dagger Hilt"}, new string[]{ "https://www.geeksforgeeks.org/mvvm-model-view-viewmodel-architecture-pattern-in-android/", "Model View ViewModel (MVVM)"},
                     }
                 }
             };
