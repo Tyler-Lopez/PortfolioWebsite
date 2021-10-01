30781b4ac267db4736c415b16eef4ca8974ed3b5
BunsenBurnsRust <77797048+BunsenBurnsRust@users.noreply.github.com>
2021-10-01 01:03:37 -0400

    Addition of project

--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -26,12 +26,23 @@ namespace Portfolio.Services
                 new Project
                 {
                     Name = "Speed Card Game",
-                    Description = "This repository is my primary focus at present and an engaging exercise in mastering the Canvas composable. The long-term goal is to create a local multi-player game where two users may play the card game of speed against one another. The featured picture is an early-development preview towards how suit shapes are being drawn; each as a solution to the tangram puzzle.",
+                    Description = "This repository is one of my primary focuses at present and an engaging exercise in mastering the Canvas composable. The long-term goal is to create a local multi-player game where two users may play the card game of speed against one another. The featured picture is an early-development preview towards how suit shapes are being drawn; each as a solution to the tangram puzzle.",
                     GithubUrl = "https://github.com/Tyler-Lopez/SpeedCardGame",
                     PictureUrl = "https://user-images.githubusercontent.com/77797048/135339050-9264c445-cbc8-4494-978c-ebf7ac25a4ee.png",
                     Tags = new List<string[]>
                     {
                        new string[]{ "https://kotlinlang.org/docs/home.html", "Kotlin"}, new string[]{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"}, new string[]{ "https://medium.com/google-developer-experts/exploring-jetpack-compose-canvas-the-power-of-drawing-8cc60815babe", "Canvas"}, new string[]{ "https://dagger.dev/hilt/", "Dagger Hilt"}, new string[]{ "https://www.geeksforgeeks.org/mvvm-model-view-viewmodel-architecture-pattern-in-android/", "Model View ViewModel (MVVM)"},
+                    }
+                },
+                new Project
+                {
+                    Name = "SprintLogger",
+                    Description = "This Android application in development will allow the user to track their fastest 400 m sprint times day-to-day. This application serves as a personal exploration into working with the Android API, specifically the GPS location service. The user should open the app, push start, then \"go fast\"! In the featured early-development preview picture, the user has just walked 400 m at approximately the average human pace of 1.4 meter/second - a very slow 400 m sprint!",
+                    GithubUrl = "https://github.com/Tyler-Lopez/SprintLogger",
+                    PictureUrl = "https://user-images.githubusercontent.com/77797048/135566877-61daeb4a-b2b2-45ec-9c88-d8aff4bbe20f.png",
+                    Tags = new List<string[]>
+                    {
+                        new string[]{ "https://kotlinlang.org/docs/home.html", "Kotlin"}, new string[]{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"}, new string[]{ "https://medium.com/google-developer-experts/exploring-jetpack-compose-canvas-the-power-of-drawing-8cc60815babe", "Canvas"}, new string[]{ "https://developer.android.com/training/location", "Location API"},
                     }
                 },
                 new Project
