640d350df842c9b2195cab037898661ee4d82711
Tyler-Lopez <tylerlopezmi@gmail.com>
2022-07-30 01:35:07 -0400

    7/30 Update

--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -11,6 +11,22 @@ namespace Portfolio.Services
             {
                 new Project
                 {
+                    Name = "Activity Art",
+                    Description =
+                        "This is an Android Application which queries the Strava API to generate highly-modular, aesthetic prints of all of an athlete's activities. " +
+                        "Filters include distance, year, month, gear, and activity type! Polylines, the series of coordinates that represent a workout, are decoded and drawn using the Android Canvas - one of my favorite tools to work with."
+                    ,
+                    GithubUrl = "https://github.com/Tyler-Lopez/StravaActivityArt",
+                    PictureUrl = "project_activityart.png",
+                    Tags = new List<string[]>
+                    {
+                        new []{ "https://kotlinlang.org/docs/home.html", "Kotlin"},
+                        new []{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"},
+                        new []{ "https://medium.com/google-developer-experts/exploring-jetpack-compose-canvas-the-power-of-drawing-8cc60815babe", "Canvas"}
+                    }
+                },
+                new Project
+                {
                     Name = "Tree Visualizer",
                     Description = 
                         "Published Android App to dynamically visualize binary trees data structures with an attractive user interface. " +
@@ -31,7 +47,7 @@ namespace Portfolio.Services
                     Name = "Computer Science Flashcards",
                     Description = "An Android educational flashcard application in-development. Questions and answers are currently hard-coded and cover various standard courses of an undergraduate computer science degree.",
                     GithubUrl = "https://github.com/Tyler-Lopez/CS-TriviaApp",
-                    PictureUrl = "project_flashcards.gif",
+                    PictureUrl = "project_flashcards.png",
                     Tags = new List<string[]>
                     {
                         new string[]{ "https://kotlinlang.org/docs/home.html", "Kotlin"}, new string[]{ "https://developer.android.com/jetpack/compose", "Jetpack Compose"}, new string[]{ "https://dagger.dev/hilt/", "Dagger Hilt"}, new string[]{ "https://www.geeksforgeeks.org/mvvm-model-view-viewmodel-architecture-pattern-in-android/", "Model View ViewModel (MVVM)"}, new string[] { "https://developer.android.com/reference/android/speech/tts/TextToSpeech", "Text-to-Speech"}
